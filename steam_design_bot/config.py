f71f5954a71293b3fa65697c79f2e7b242b7d009
gololo700-del <gololo700@gmail.com>
2026-06-09 20:16:52 +0300

    Add files via upload

---
+++ steam_design_bot/config.py
@@ -0,0 +1,14 @@
+# ==========================================
+#   НАСТРОЙКИ БОТА — ЗАПОЛНИ ПЕРЕД ЗАПУСКОМ
+# ==========================================
+
+import os
+
+# Токен бота от @BotFather
+BOT_TOKEN = os.getenv("8735141821:AAFwHVuQViNWz3OF9clXfPytfiVlpH-oQJo")
+
+# Твой Telegram ID (число) — узнай у @userinfobot
+ADMIN_ID = 5460379972  # <- замени на свой ID
+
+# Твой Telegram username (без @)
+ADMIN_USERNAME = "ryzyni"
